2b6f8aad94446a6c49aae5e87bd69081faa525fd
Eric Schaal <erickeir.s@gmail.com>
2018-08-31 16:44:26 -0400

    Update RNCombinedChartView.swift

--- ios/ReactNativeCharts/combine/RNCombinedChartView.swift
+++ ios/ReactNativeCharts/combine/RNCombinedChartView.swift
@@ -16,17 +16,36 @@ class RNCombinedChartView: RNBarLineChartViewBase {
     
     override var dataExtract: DataExtract {
         return _dataExtract
-    }    
-
+    }
+  
+  
+  override func setData(_ data: NSDictionary) {
+    let json = BridgeUtils.toJson(data)
+    chart.data = dataExtract.extract(json)
+    if let chartData = chart.data {
+      let chartRange = chartData.xMax - chartData.xMin
+      let minRange = min(chartRange, 5*60)
+      chart.setVisibleXRangeMinimum(minRange) // 10mn
+      chart.setVisibleXRangeMaximum(12*3600) // 12h
+    }
+  }
+  
     override init(frame: CoreGraphics.CGRect) {
 
         self._chart = CombinedChartView(frame: frame)
         self._dataExtract = CombinedDataExtract()
 
         super.init(frame: frame)
+      
 
         self._chart.delegate = self
         self.addSubview(_chart)
+      
+      let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chartViewLongPress(gesture:)))
+      gestureRecognizer.minimumPressDuration = 0.25;
+      self.addGestureRecognizer(gestureRecognizer);
+      
+      self._chart.highlightFullBarEnabled = true;
     }
 
     required init?(coder aDecoder: NSCoder) {
