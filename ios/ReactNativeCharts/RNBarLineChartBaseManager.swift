e19b6bf9c987bc7d623a4aa9f87b1804340731fd
Eric Schaal <eric.schaal@nxtsens.com>
2019-03-11 11:17:48 -0400

    zoom via bridge

--- ios/ReactNativeCharts/RNBarLineChartBaseManager.swift
+++ ios/ReactNativeCharts/RNBarLineChartBaseManager.swift
@@ -17,49 +17,49 @@ extension RNBarLineChartBaseManager {
       (view.chart as! BarLineChartViewBase).moveViewToX(xValue.doubleValue);
     }
   }
-  
+
   func _moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).moveViewTo(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String));
     }
   }
-  
+
   func _zoomViewXAnimated(_ reactTag: NSNumber, scaleX: NSNumber, axisDependency: NSString) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
-      (view.chart as! BarLineChartViewBase).zoom(scaleX: CGFloat(truncating: scaleX), scaleY: 1.0, xValue: Double(view.chart.center.x), yValue: Double(view.chart.center.y), axis: BridgeUtils.parseAxisDependency(axisDependency as String))
+      (view.chart as! BarLineChartViewBase).zoom(scaleX: scaleX as! CGFloat, scaleY: CGFloat(1.0), xValue: Double(view.chart.center.x), yValue: Double(view.chart.center.y), axis: BridgeUtils.parseAxisDependency(axisDependency as String))
     }
   }
-  
+
   func _moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).moveViewToAnimated(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String), duration: duration.doubleValue / 1000.0);
     }
   }
-  
+
   func _centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).centerViewTo(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String));
     }
   }
-  
+
   func _centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).centerViewToAnimated(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String), duration: duration.doubleValue / 1000.0);
     }
   }
-  
+
   func _fitScreen(_ reactTag: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).fitScreen();
     }
   }
-  
+
   func _highlights(_ reactTag: NSNumber, config: NSArray) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
       let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
