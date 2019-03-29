1697b81f4ef7b24651b8fd7b45e27373b332c00a
Eric Schaal <eric.schaal@nxtsens.com>
2019-03-29 15:18:19 -0400

    iOS fix

--- ios/ReactNativeCharts/combine/RNCombinedChartManager.swift
+++ ios/ReactNativeCharts/combine/RNCombinedChartManager.swift
@@ -7,51 +7,52 @@ import UIKit
 @objc(RNCombinedChartManager)
 @objcMembers
 open class RNCombinedChartManager: RCTViewManager, RNBarLineChartBaseManager {
-  var _bridge: RCTBridge? {get{return self.bridge}}
-  
-  override open func view() -> UIView! {
-    let ins = RNCombinedChartView()
-    return ins;
-  }
-
-  override open static func requiresMainQueueSetup() -> Bool {
-    return true;
-  }
-  
-  func moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
-    (self as RNBarLineChartBaseManager)._moveViewToX(reactTag, xValue: xValue)
-  }
-  
-  func moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
-    (self as RNBarLineChartBaseManager)._moveViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
-  }
-  
-  func moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
-    (self as RNBarLineChartBaseManager)._moveViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
-  }
-  
-  func zoomViewXAnimated(_ reactTag: NSNumber, scaleX: NSNumber, axisDependency: NSString) {
-    (self as RNBarLineChartBaseManager)._zoomViewXAnimated(reactTag, scaleX: scaleX, axisDependency: axisDependency)
-  }
-  
-  func centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
-    (self as RNBarLineChartBaseManager)._centerViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
-  }
-  
-  func centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
-    (self as RNBarLineChartBaseManager)._centerViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
-  }
-  
-  func fitScreen(_ reactTag: NSNumber) {
-    (self as RNBarLineChartBaseManager)._fitScreen(reactTag)
-  }
-  
-  func highlights(_ reactTag: NSNumber, config: NSArray) {
-    (self as RNBarLineChartBaseManager)._highlights(reactTag, config: config)
-  }
-
-  func setDataAndLockIndex(_ reactTag: NSNumber, data: NSDictionary) {
-    (self as RNBarLineChartBaseManager)._setDataAndLockIndex(reactTag, data: data)
-  }
-
+    var _bridge: RCTBridge? {get{return self.bridge}}
+    
+    override open func view() -> UIView! {
+        let ins = RNCombinedChartView()
+        return ins;
+    }
+    
+    override open static func requiresMainQueueSetup() -> Bool {
+        return true;
+    }
+    
+    func moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
+        (self as RNBarLineChartBaseManager)._moveViewToX(reactTag, xValue: xValue)
+    }
+    
+    func moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._moveViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
+    }
+    
+    func moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
+        (self as RNBarLineChartBaseManager)._moveViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
+    }
+    
+    func _zoomViewEnd(_ reactTag: NSNumber, scaleX: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._zoomViewEnd(reactTag, scaleX: scaleX, axisDependency: axisDependency)
+        
+    }
+    
+    func centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._centerViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
+    }
+    
+    func centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
+        (self as RNBarLineChartBaseManager)._centerViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
+    }
+    
+    func fitScreen(_ reactTag: NSNumber) {
+        (self as RNBarLineChartBaseManager)._fitScreen(reactTag)
+    }
+    
+    func highlights(_ reactTag: NSNumber, config: NSArray) {
+        (self as RNBarLineChartBaseManager)._highlights(reactTag, config: config)
+    }
+    
+    func setDataAndLockIndex(_ reactTag: NSNumber, data: NSDictionary) {
+        (self as RNBarLineChartBaseManager)._setDataAndLockIndex(reactTag, data: data)
+    }
+    
 }
--- ios/ReactNativeCharts/line/RNLineChartManager.swift
+++ ios/ReactNativeCharts/line/RNLineChartManager.swift
@@ -7,47 +7,51 @@ import UIKit
 @objc(RNLineChartManager)
 @objcMembers
 open class RNLineChartManager: RCTViewManager, RNBarLineChartBaseManager {
-  var _bridge: RCTBridge? {get{return self.bridge}}
-  
-  override open func view() -> UIView! {
-    let ins = RNLineChartView()
-    return ins;
-  }
-
-  override open static func requiresMainQueueSetup() -> Bool {
-    return true;
-  }
-  
-  func moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
-    (self as RNBarLineChartBaseManager)._moveViewToX(reactTag, xValue: xValue)
-  }
-  
-  func moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
-    (self as RNBarLineChartBaseManager)._moveViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
-  }
-  
-  func moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
-    (self as RNBarLineChartBaseManager)._moveViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
-  }
-  
-  func centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
-    (self as RNBarLineChartBaseManager)._centerViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
-  }
-  
-  func centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
-    (self as RNBarLineChartBaseManager)._centerViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
-  }
-  
-  func fitScreen(_ reactTag: NSNumber) {
-    (self as RNBarLineChartBaseManager)._fitScreen(reactTag)
-  }
-  
-  func highlights(_ reactTag: NSNumber, config: NSArray) {
-    (self as RNBarLineChartBaseManager)._highlights(reactTag, config: config)
-  }
-
-  func setDataAndLockIndex(_ reactTag: NSNumber, data: NSDictionary) {
-    (self as RNBarLineChartBaseManager)._setDataAndLockIndex(reactTag, data: data)
-  }
-
+    var _bridge: RCTBridge? {get{return self.bridge}}
+    
+    override open func view() -> UIView! {
+        let ins = RNLineChartView()
+        return ins;
+    }
+    
+    override open static func requiresMainQueueSetup() -> Bool {
+        return true;
+    }
+    
+    func moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
+        (self as RNBarLineChartBaseManager)._moveViewToX(reactTag, xValue: xValue)
+    }
+    
+    func moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._moveViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
+    }
+    
+    func moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
+        (self as RNBarLineChartBaseManager)._moveViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
+    }
+    
+    func centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._centerViewTo(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency)
+    }
+    
+    func zoomViewEnd(_ reactTag: NSNumber, scaleX: NSNumber, axisDependency: NSString) {
+        (self as RNBarLineChartBaseManager)._zoomViewEnd(reactTag, scaleX: scaleX, axisDependency: axisDependency)
+    }
+    
+    func centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
+        (self as RNBarLineChartBaseManager)._centerViewToAnimated(reactTag, xValue: xValue, yValue: yValue, axisDependency: axisDependency, duration: duration)
+    }
+    
+    func fitScreen(_ reactTag: NSNumber) {
+        (self as RNBarLineChartBaseManager)._fitScreen(reactTag)
+    }
+    
+    func highlights(_ reactTag: NSNumber, config: NSArray) {
+        (self as RNBarLineChartBaseManager)._highlights(reactTag, config: config)
+    }
+    
+    func setDataAndLockIndex(_ reactTag: NSNumber, data: NSDictionary) {
+        (self as RNBarLineChartBaseManager)._setDataAndLockIndex(reactTag, data: data)
+    }
+    
 }
